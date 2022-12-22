5d185b8325f33cb21f6552092eda63c8c219203d
ankitpandey12199 <101467904+ankitpandey12199@users.noreply.github.com>
2022-12-22 21:26:14 +0530

    Time: 1 ms (99.98%), Space: 43.1 MB (82.23%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -3,26 +3,26 @@ class Solution {
         Arrays.sort(nums);
           List<List<Integer>>list=new ArrayList<>();
         boolean arr[]=new boolean[nums.length];
-        helper(nums,new ArrayList<>(),list,0,arr);
+        helper(nums,new ArrayList<>(),list,0,arr,arr.length);
         return list;
     }
-    public void helper(int arr[],List<Integer>list,List<List<Integer>>ans,int index,boolean visited[])
+    public void helper(int arr[],List<Integer>list,List<List<Integer>>ans,int index,boolean visited[],int n)
         
     {  
-        if(index==arr.length){
+        if(index==n){
         ans.add(new ArrayList<Integer>(list));
         return;
     }
-        for(int i=0;i<arr.length;i++)
+        for(int i=0;i<n;i++)
         {  
             if(!visited[i])
             { 
             visited[i]=true;
             list.add(arr[i]);
-            helper(arr,list,ans,index+1,visited);
+            helper(arr,list,ans,index+1,visited,n);
             list.remove(list.size()-1);
             visited[i]=false;
-            while(i<arr.length-1 && arr[i]==arr[i+1])i++;    
+            while(i<n-1 && arr[i]==arr[i+1])i++;    
             }
         }
     }
