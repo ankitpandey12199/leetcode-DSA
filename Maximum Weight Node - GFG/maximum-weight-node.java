0ac48f7bb9ca1dc74755eaf2d5a84e621856b6da
ankitpandey12199 <101467904+ankitpandey12199@users.noreply.github.com>
2023-01-20 10:39:10 +0530

    Added solution - LeetHub

---
+++ Maximum Weight Node - GFG/maximum-weight-node.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class GFG{
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        Long nextLong(){
+            return Long.parseLong(next());
+        }
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int N = sc.nextInt();
+            int Edge[] = new int[N];
+            for(int i = 0; i < N; i++)
+                Edge[i] = sc.nextInt();
+            Solution ob = new Solution();
+            int ans = ob.maxWeightCell(N, Edge);
+            out.println(ans);            
+        }
+        out.flush();
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+//User function Template for Java
+
+class Solution{
+    public int maxWeightCell(int N, int Edge[]){
+        int arr[]=new int[N];
+        for(int i=0;i<N;i++){
+            int e = Edge[i];
+            if(e!=-1){
+                arr[e]+=i;
+            }
+       }
+       int max=0;
+       int ans=0;
+       for(int i=0;i<N;i++){
+           if(max<=arr[i]){
+               max=arr[i];
+               ans=i;
+           }
+
+       }
+       return ans;
+    }
+}
