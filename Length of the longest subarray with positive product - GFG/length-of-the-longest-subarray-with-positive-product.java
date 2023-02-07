b4c5664693a98071c904699877ddad8ecf6faf59
ankitpandey12199 <101467904+ankitpandey12199@users.noreply.github.com>
2023-02-07 10:11:22 +0530

    Added solution - LeetHub

---
+++ Length of the longest subarray with positive product - GFG/length-of-the-longest-subarray-with-positive-product.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n =Integer.parseInt(in.readLine().trim());
+            int arr[] = new int[n];
+            String s[]=in.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+             arr[i] = Integer.parseInt(s[i]);
+             
+            out.println(new Solution().maxLength(arr, n)); 
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution{
+   int maxLength(int arr[], int n) { 
+        int neg_found=-1,zero_found=-1,neg=0,max=0;
+        for(int i=0;i<n;i++){
+           if(arr[i]<0){
+               neg++;
+               if(neg_found==-1)
+                neg_found=i;
+           }
+           if(arr[i]==0){
+               zero_found=i;
+               neg_found=-1; 
+               neg=0; 
+           }
+           if(neg%2==0){
+               max=Math.max(i-zero_found,max);
+           }else 
+                max=Math.max(i-neg_found,max);
+        }
+        return max;
+    }
+   
+}
