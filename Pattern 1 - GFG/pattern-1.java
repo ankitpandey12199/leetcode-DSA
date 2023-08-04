a344a70523cddde0f33a44c26c283a3fa3244a92
ankitpandey12199 <101467904+ankitpandey12199@users.noreply.github.com>
2023-08-04 22:27:01 +0530

    Added solution - LeetHub

--- Pattern 1 - GFG/pattern-1.java
+++ Pattern 1 - GFG/pattern-1.java
@@ -25,9 +25,7 @@ class Solution {
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
-            {
-                System.out.print("* ");
-            }
+            System.out.print("* ");
             System.out.println();
         }
     }
